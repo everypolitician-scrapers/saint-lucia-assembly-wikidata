3bfdb422fe8bf433030c5e881280411b5dce81e7
Tony Bowden <tony@tmtm.com>
2016-03-06 09:04:02 +0000

    Modernise scraper

--- scraper.rb
+++ scraper.rb
@@ -1,32 +1,7 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require 'json'
-require 'pry'
-require 'rest-client'
-require 'scraperwiki'
 require 'wikidata/fetcher'
-require 'mediawiki_api'
 
-def candidates
-  morph_api_url = 'https://api.morph.io/tmtmtmtm/saint-lucia-assembly-wikipedia/data.json'
-  morph_api_key = ENV["MORPH_API_KEY"]
-  result = RestClient.get morph_api_url, params: {
-    key: morph_api_key,
-    query: "select DISTINCT(wikipedia__en) as wikiname from data"
-  }
-  JSON.parse(result, symbolize_names: true)
-end
-
-WikiData.ids_from_pages('en', candidates.map { |c| c[:wikiname] }).each_with_index do |p, i|
-  puts i if (i % 50).zero?
-  data = WikiData::Fetcher.new(id: p.last).data('en') rescue nil
-  unless data
-    warn "No data for #{p}"
-    next
-  end
-  ScraperWiki.save_sqlite([:id], data)
-end
-
-warn RestClient.post ENV['MORPH_REBUILDER_URL'], {} if ENV['MORPH_REBUILDER_URL']
-
+names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/saint-lucia-assembly-wikipedia', column: 'wikipedia__en')
+EveryPolitician::Wikidata.scrape_wikidata(names: { en: names })
